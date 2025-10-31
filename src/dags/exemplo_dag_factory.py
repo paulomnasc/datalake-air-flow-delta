e41eab0779dc80d0c9878e4e444b02d6709e866c
Paulo Nascimento <paulomnascc@gmail.com>
2025-10-31 11:37:06 -0300

    corrigido o path do exemplo_dag_factory.yml.

--- src/dags/exemplo_dag_factory.py
+++ src/dags/exemplo_dag_factory.py
@@ -5,11 +5,11 @@ from pathlib import Path
 # from airflow import DAG
 from dagfactory import load_yaml_dags
 
-DEFAULT_CONFIG_ROOT_DIR = "/usr/local/airflow/dags/"
+DEFAULT_CONFIG_ROOT_DIR = "/opt/airflow/dags/"
 
 CONFIG_ROOT_DIR = Path(os.getenv("CONFIG_ROOT_DIR", DEFAULT_CONFIG_ROOT_DIR))
 
-config_file = str(CONFIG_ROOT_DIR / "examplo_dag_factory.yml")
+config_file = str(CONFIG_ROOT_DIR / "exemplo_dag_factory.yml")
 
 load_yaml_dags(
     globals_dict=globals(),

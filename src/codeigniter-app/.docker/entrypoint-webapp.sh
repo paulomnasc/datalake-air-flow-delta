96c5a7a477598fd2da5e5ad6e492cb53eb509f49
Paulo Nascimento <paulomnascc@gmail.com>
2025-11-02 15:22:00 -0300

    Resolvido o problema do webapp não iniciar devido a erros de permissão e formatação de arquivo no entrypoint.

--- src/codeigniter-app/.docker/entrypoint-webapp.sh
+++ src/codeigniter-app/.docker/entrypoint-webapp.sh
@@ -16,5 +16,12 @@ fi
 cp /tmp/htaccess-no-ssl /var/www/html/.htaccess
 echo "✅ .htaccess substituído para desabilitar o redirecionamento HTTPS."
 
+# Remove o conteúdo das pastas de cache e logs (Ignora erro se a pasta for a raiz)
+rm -rf /var/www/html/writable/cache/*
+rm -rf /var/www/html/writable/logs/*
+
+# 2. Garante as permissões de escrita recursivamente.
+chmod -R 777 /var/www/html/writable
+
 # 3. Executa o comando principal do contêiner (inicia o Apache)
 exec "$@"

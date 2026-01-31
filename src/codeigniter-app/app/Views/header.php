34031ff9b0cc2b3c0121e512001f58aa37cba4ab
Paulo <paulomnasc@gmail.com>
2026-01-31 12:54:44 +0000

    Ajustes na configuração do Nginx e atualizações na landing page e cabeçalho do site.

--- src/codeigniter-app/app/Views/header.php
+++ src/codeigniter-app/app/Views/header.php
@@ -168,11 +168,7 @@ if (isset($_SESSION['usuario_logado']) && $_SESSION['usuario_logado'] == 1) {
 <html lang="pt-BR">
 <head>
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KD72GH3P');</script>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KD72GH3P');</script>
     <!-- End Google Tag Manager -->
     
     <!-- Google Analytics 4 - DEVE SER O PRIMEIRO SCRIPT -->

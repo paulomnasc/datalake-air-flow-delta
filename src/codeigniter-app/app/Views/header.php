30fc3bae8d84d4257c444178a4efd2ba034be45e
Paulo <paulomnasc@gmail.com>
2026-01-31 15:53:19 +0000

    Ajuste de visibilidade autorizada do menu cursos

--- src/codeigniter-app/app/Views/header.php
+++ src/codeigniter-app/app/Views/header.php
@@ -639,7 +639,7 @@ if (isset($_SESSION['usuario_logado']) && $_SESSION['usuario_logado'] == 1) {
             <a href="/" class="nav-link px-4 px-lg-5" title="Início">
                 <i class="fas fa-home" style="font-size: 28px; vertical-align: middle;"></i>
             </a>
-            <?php echo anchor("curso/modulo1", "CURSOS", ['class' => 'nav-link px-4 px-lg-5']) ?>
+            
 
 
             <div id="itens-menu-outros" class="navbar-nav ms-auto p-4 p-lg-0">
@@ -658,7 +658,14 @@ if (isset($_SESSION['usuario_logado']) && $_SESSION['usuario_logado'] == 1) {
                 $servicesBlocked = isset($_SESSION['subscription_services_blocked']) && $_SESSION['subscription_services_blocked'];
                 ?>
 
+                <?php echo anchor("curso/modulo1", "CURSOS", ['class' => 'nav-link px-4 px-lg-5']) ?>
+                
+
                 <li class="nav-item dropdown <?= $servicesBlocked ? 'disabled' : '' ?>">
+
+
+                    
+
                     <?php if ($servicesBlocked): ?>
                         <a class="nav-link dropdown-toggle" href="#" style="opacity: 0.5; cursor: not-allowed;" 
                            title="Renovar assinatura para acessar os serviços" onclick="event.preventDefault(); alert('⚠️ Assinatura expirada!\n\nPara acessar os serviços, renove sua assinatura.')">
